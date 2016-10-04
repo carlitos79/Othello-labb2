49ab191352530e246ce820a52b2746e4d12996b8
optimusprime <optimusprime@DESKTOP-KMK55FI>
2016-10-04 09:18:03 +0200

    Jag skapade bara en package med player klasserna

---
+++ src/participants/Player.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package participants;
+
+/**
+ *
+ * @author optimusprime
+ */
+public  abstract class Player {
+        
+    /*
+     * Jag tänker mig att man ska kanske ge id till spelaren här eller någonstans 
+     * genom att ha en statisk int variabel som ökar varje gång en spelare skulle skapas?
+     */
+}
